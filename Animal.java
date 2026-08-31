dce5019d7c75a959f49c94533ed09e94491bef45
matiasah2117 <matias.ah2117@gmail.com>
2026-08-30 21:21:20 -0400

    se añade objeto Animal

---
+++ Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    string nombre;
+    string clasificacion;
+    init edad;
+    string habla 
+
+
+
+    //métodos
+    void comer(){
+        System.out.println(nombre + "se está alimentando")
+  
+    }
+    void jugar (){
+
+        System.out.println(nombre + "está comiendo")
+    }
+
+
+}     
